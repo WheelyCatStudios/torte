d93c292754405ea82400773fc9fafb5a77a139ee
realQuartzi <nightmaresdev@gmail.com>
2020-12-18 21:42:47 +0100

    Base Menu Scripts Added
    Includes:
    - Base Menu Script
    - Base Mouse Over Event Script

---
+++ Assets/Scripts/Menu/Detail/ObjectHover.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ObjectHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public GameObject pin;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        pin.SetActive(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        pin.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Menu/Menu.cs
@@ -0,0 +1,43 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Menu : MonoBehaviour
+{
+
+    // Activate and Deactivate UI Gameobjects
+    public void OCObject(GameObject obj)
+    {
+        if (obj.activeInHierarchy)
+        {
+            obj.SetActive(false);
+        }
+        else
+        {
+            obj.SetActive(true);
+        }
+
+    }
+
+    // Load Scene
+    public void LoadScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+    public void LoadScene(int scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    // Close Editor && Close Game
+    public void ExitGame()
+    {
+        Debug.Log("Exiting Game");
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#endif
+
+        Application.Quit();
+    }
+}
