b078350639c81b8ed34539138762c98b6fedee68
Sean Lynch <66145294+funisinfinite@users.noreply.github.com>
2020-12-21 12:19:45 -0500

    Added new input sys to Entity script

--- Assets/EntityMovmentScript.cs
+++ Assets/EntityMovmentScript.cs
@@ -1,18 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class EntityMovmentScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public MasterInput controls = new MasterInput();
 
-    // Update is called once per frame
-    void Update()
+
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    void Awake()
     {
-        
+        controls.Player.Movment.performed += ctx => Move(ctx.ReadValue<Vector2>());
+    }
+    
+    private void OnEnable() {
+        controls.Enable();
+    }
+    private void OnDisable() {
+        controls.Disable();
+    }
+    void Move(Vector2 direction)
+    {
+        Debug.Log("Move " + direction);
     }
 }
