1dcb80b6bce886eeec38fe34b2ede2add87b7457
realQuartzi <nightmaresdev@gmail.com>
2020-12-22 00:06:34 +0100

    Menu Upgrade
    Added Menu as Build Scene >(index:0)
    Included Testing Font "Torte"
    New Testing Sound "Boot"
    
    New Fancy Animation Events for Hover and Click Events

--- Assets/Scripts/Menu/Detail/ObjectHover.cs
+++ Assets/Scripts/Menu/Detail/ObjectHover.cs
@@ -3,15 +3,46 @@ using UnityEngine.EventSystems;
 
 public class ObjectHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    [Header("Animation")]
+    public Animator anim;
+
+    [Header("Visual")]
     public GameObject pin;
+
+    [Header("Sound")]
+    public AudioClip sound;
+
+    bool hover;
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        pin.SetActive(true);
+        hover = true;
+
+        if(anim != null)
+        {
+            anim.SetBool("Hover", hover);
+        }
+
+        if(pin != null)
+        {
+            pin.SetActive(true);
+        }
+
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        pin.SetActive(false);
+        hover = false;
+
+        if (anim != null)
+        {
+            anim.SetBool("Hover", hover);
+        }
+
+        if (pin != null)
+        {
+            pin.SetActive(false);
+        }
+
     }
 }
--- Assets/Scripts/Menu/Menu.cs
+++ Assets/Scripts/Menu/Menu.cs
@@ -1,10 +1,14 @@
-﻿using UnityEditor;
+﻿using System.Collections;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
 public class Menu : MonoBehaviour
 {
+    [Header("Fancy")]
+    public Animator anim;
+    public AudioSource source;
+    public AudioClip bootClip;
 
     // Activate and Deactivate UI Gameobjects
     public void OCObject(GameObject obj)
@@ -21,22 +25,39 @@ public class Menu : MonoBehaviour
     }
 
     // Load Scene
-    public void LoadScene(string scene)
-    {
-        SceneManager.LoadScene(scene);
-    }
     public void LoadScene(int scene)
     {
-        SceneManager.LoadScene(scene);
+        StartCoroutine(LoadSceneFancy(scene));
     }
 
     // Close Editor && Close Game
     public void ExitGame()
     {
+        StartCoroutine(ExitGameFancy());
+    }
+
+    IEnumerator LoadSceneFancy(int scene)
+    {
+        source.PlayOneShot(bootClip);
+        anim.SetTrigger("Start");
+
+        yield return new WaitForSeconds(6f);
+
+        SceneManager.LoadScene(scene);
+    }
+
+    IEnumerator ExitGameFancy()
+    {
+        source.PlayOneShot(bootClip);
+        anim.SetTrigger("Exit");
+
+        yield return new WaitForSeconds(6f);
+
         Debug.Log("Exiting Game");
-#if UNITY_EDITOR
-        EditorApplication.ExitPlaymode();
-#endif
+
+        #if UNITY_EDITOR
+            EditorApplication.ExitPlaymode();
+        #endif
 
         Application.Quit();
     }
