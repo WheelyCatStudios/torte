78e3f86021bce2bd6f363e9d61613b17918e9917
realQuartzi <nightmaresdev@gmail.com>
2020-12-22 19:48:13 +0100

    Test Map Prototyping
    Create Prototype Map
    Contains:
    - Prototype Tile Sprites
    - Prototype Rule Tile
    - 2D Extras Package

---
+++ Assets/Scripts/Prototype/Testing/MoveDebug.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveDebug : MonoBehaviour
+{
+
+    Rigidbody2D rigid;
+
+    [Range(-1,1)]
+    public float x;
+    [Range(-1,1)]
+    public float y;
+
+    public float speed;
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+
+        Debug.Log("X Input: " + x);
+        Debug.Log("Y Input: " + y);
+    }
+
+    private void FixedUpdate()
+    {
+        rigid.velocity = new Vector2(x, y) * speed;
+    }
+}
