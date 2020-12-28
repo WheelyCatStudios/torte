aedc80d354bb2318237c56d494ca0232fbcba637
realQuartzi <nightmaresdev@gmail.com>
2020-12-29 00:06:36 +0100

    Updated Merge
    Updated Sprites added to the Test Scene.
    Menu Start event is connected with Test Scene.
    Fixed UI Scaling

--- Assets/Scripts/Menu/Menu.cs
+++ Assets/Scripts/Menu/Menu.cs
@@ -41,7 +41,7 @@ public class Menu : MonoBehaviour
         source.PlayOneShot(bootClip);
         anim.SetTrigger("Start");
 
-        yield return new WaitForSeconds(6f);
+        yield return new WaitForSeconds(2f);
 
         SceneManager.LoadScene(scene);
     }
