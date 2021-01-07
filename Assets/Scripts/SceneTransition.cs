feacc5d97c8d7fec532a7b57bd4d6416a971a36f
Sean Lynch <66145294+funisinfinite@users.noreply.github.com>
2021-01-06 20:57:34 -0500

    Added direction and updated approachThreshold

--- Assets/Scripts/SceneTransition.cs
+++ Assets/Scripts/SceneTransition.cs
@@ -6,9 +6,11 @@ using UnityEngine.SceneManagement;
 public class SceneTransition : MonoBehaviour
 {
     public string sceneName; //Required Field; Case Sensitve; Please use full path instead of name
-    public float approachThreshold = 1f;
+    public float approachThreshold = 10f;
     private float departThreshold;
-    
+    private Vector2 direction;
+    private float distance;
+
 
 
     
@@ -16,12 +18,11 @@ public class SceneTransition : MonoBehaviour
     void Start()
     {
         departThreshold = approachThreshold * 1.5f;
+        direction = GameObject.FindWithTag("Player").GetComponent<Rigidbody2D>().velocity; //Grab velocity of player and set a Vector2
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void LateUpdate() {
+        //distance = Vector2.Distance(direction, )    
     }
 
 
