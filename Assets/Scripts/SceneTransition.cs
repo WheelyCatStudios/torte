1a62491ccf3c3233bb5745030808325e26b816c4
Sean Lynch <66145294+funisinfinite@users.noreply.github.com>
2021-01-06 20:40:34 -0500

    Added simple base scene transition code

--- Assets/Scripts/SceneTransition.cs
+++ Assets/Scripts/SceneTransition.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneTransition : MonoBehaviour
 {
+    public string sceneName; //Required Field; Case Sensitve
+
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +23,9 @@ public class SceneTransition : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log(other.tag);
+        if (other.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(sceneName);
+        }
     }
 }
