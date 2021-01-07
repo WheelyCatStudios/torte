21a126163276bc287630c0906ac84b57112e7b65
Sean Lynch <66145294+funisinfinite@users.noreply.github.com>
2021-01-06 20:52:24 -0500

    Started adding variables

--- Assets/Scripts/SceneTransition.cs
+++ Assets/Scripts/SceneTransition.cs
@@ -5,13 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class SceneTransition : MonoBehaviour
 {
-    public string sceneName; //Required Field; Case Sensitve
+    public string sceneName; //Required Field; Case Sensitve; Please use full path instead of name
+    public float approachThreshold = 1f;
+    private float departThreshold;
+    
+
 
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        departThreshold = approachThreshold * 1.5f;
     }
 
     // Update is called once per frame
