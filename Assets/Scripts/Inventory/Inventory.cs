d2c23f22b24e2255e33192a56df0d6dc3f67c849
Gal Chen <galdchen@gmail.com>
2021-01-07 23:21:49 -0800

    Added Comments
    added lots of completly un-needed comments. There is no way these will be usefull to anybody. This is for the sake of code consistency only.

--- Assets/Scripts/Inventory/Inventory.cs
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -4,19 +4,40 @@ using UnityEngine;
 /// The Inventory System
 /// </summary>
 namespace InventorySystem {
+    /// <summary>
+    /// The Inventory class serves as a container for InventoryItem objects with methods to manage the contained objects.
+    /// Attach an inventory to any GameObject that has an inventory i.e. Todd, chests, trash cans, NPCs, etc.
+    /// </summary>
     public class Inventory : MonoBehaviour {
+        /// <summary>
+        /// This is a list of InventoryItem objects (AKA an inventory).
+        /// </summary>
         private List<InventoryItem> _inventory = new List<InventoryItem>();
 
+        /// <summary>
+        /// Add an item to the inventory.
+        /// </summary>
+        /// <param name="item">The item you want to add.</param>
         public void AddItem(InventoryItem item)
         {
             _inventory.Add(item);
         }
 
+        /// <summary>
+        /// Remove an item from the inventory.
+        /// </summary>
+        /// <param name="item">The item you want to remove.</param>
+        /// <returns>Returns true if item was removed.</returns>
          private bool RemoveItem(InventoryItem item)
         {
             if (item.Removable) { _inventory.Remove(item); }
             return item.Removable;
         }
+        /// <summary>
+        /// Remove an item from inventory.
+        /// </summary>
+        /// <param name="i">index within the inventory of the item you want to remove from the inventory.</param>
+        /// <returns>Returns true if item was removed.</returns>
         private bool RemoveItem(int i)
         {
             if (_inventory[i].Removable) { 
@@ -29,6 +50,11 @@ namespace InventorySystem {
             }
         }
 
+        /// <summary>
+        /// Drop an item from your inventory to the ground.
+        /// </summary>
+        /// <param name="item">The item you'd like to drop.</param>
+        /// <returns>Returns true if dropped.</returns>
         public bool DropItem(InventoryItem item)
         {
             if (RemoveItem(item))
@@ -42,6 +68,11 @@ namespace InventorySystem {
                 return false;
             }
         }
+        /// <summary>
+        /// Drop an item from the inventory to the ground.
+        /// </summary>
+        /// <param name="i">The index within the inventory of the item you'd like to drop</param>
+        /// <returns>Returns true if dropped.</returns>
         public bool DropItem(int i)
         {
             if (RemoveItem(_inventory[i]))
@@ -55,6 +86,12 @@ namespace InventorySystem {
             }
         }
 
+        /// <summary>
+        /// Destroys an item within the inventory.
+        /// This may seem similar to <a cref="RemoveItem(InventoryItem)"/>
+        /// </summary>
+        /// <param name="item">The item to destroy.</param>
+        /// <returns>Returns true if item was destoryed.</returns>
         public bool DestroyItem(InventoryItem item)
         {
             if (RemoveItem(item))
@@ -67,6 +104,12 @@ namespace InventorySystem {
                 return false;
             }
         }
+        /// <summary>
+        /// Destroys an item within the inventory.
+        /// This may seem similar to <a cref="RemoveItem(int)"/>
+        /// </summary>
+        /// <param name="i">The index within the inventory of the item you'd like to destroy.</param>
+        /// <returns>Returns true if the item was destroyed.</returns>
         public bool DestroyItem(int i)
         {
             if (RemoveItem(_inventory[i]))
@@ -80,6 +123,11 @@ namespace InventorySystem {
             }
         }
 
+        /// <summary>
+        /// Use an item.
+        /// </summary>
+        /// <param name="item">The item that will be used.</param>
+        /// <returns>Returns true is item was used.</returns>
         public bool UseItem(InventoryItem item)
         {
             if (item.Usable)
@@ -92,6 +140,11 @@ namespace InventorySystem {
                 return false;
             }
         }
+        /// <summary>
+        /// Use an item.
+        /// </summary>
+        /// <param name="i">Index of the item within the inventory that you'd like to use.</param>
+        /// <returns>Returns true if item was used.</returns>
         public bool UseItem(int i)
         {
             if (_inventory[i].Usable)
