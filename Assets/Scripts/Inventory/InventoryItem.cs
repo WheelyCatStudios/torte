eb33c469aab8e76528a1e4c790296d0c74e687f9
Gal Chen <galdchen@gmail.com>
2021-01-08 00:35:12 -0800

    Use addressable assets.
    updated script to utilize addressable assets.

--- Assets/Scripts/Inventory/InventoryItem.cs
+++ Assets/Scripts/Inventory/InventoryItem.cs
@@ -1,6 +1,8 @@
 using System;
 using System.IO;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+
 /// <summary>
 /// The Inventory System
 /// </summary>
@@ -8,18 +10,38 @@ namespace InventorySystem
 {
     public class InventoryItem{
 
-        public InventoryItem(string name = _defaultItemName, string description = _defaultItemDescription, int value = _defaultItemValue, int uses = _defaultItemUses, bool useable = _defaultItemUseable, bool removable = _defaultItemRemovable, bool destroyable = _defaultItemDestroyable){ //, function effect = _defaultItemEffect){
-            _itemName = name;
-            _itemDescription = description;
-            _itemValue = value;
-            _itemUses = uses;
-            //_itemIcon = icon;
-            //_itemPicture = picture;
-            _itemUseable = useable;
-            _itemRemovable = removable;
-            _itemDestroyable = destroyable;
-            //_itemEffect = effect;
+        public InventoryItem(
+            string name = _defaultName,
+            string description = _defaultDescription,
+            int value = _defaultValue,
+            int uses = _defaultUses,
+            string iconLocation = _defaultIconLocation,
+            string pictureLocation = _defaultPictureLocation,
+            string spriteLocation = _defaultSpriteLocation,
+            bool useable = _defaultUseable,
+            bool removable = _defaultRemovable,
+            bool destroyable = _defaultDestroyable
+            ){
+            _name = name;
+            _description = description;
+            _value = value;
+            _uses = uses;
+            //_iconLocation = iconLocation;
+            //_pictureLocation = pictureLocation;
+            //_spriteLocation = spriteLocation;
+            _useable = useable;
+            _removable = removable;
+            _destroyable = destroyable;
+            //_Effect = effect;
+
+            Addressables.LoadAssetAsync<Sprite>(iconLocation).Completed += OnGotIcon;
+            Addressables.LoadAssetAsync<Sprite>(pictureLocation).Completed += OnGotPic;
+            Addressables.LoadAssetAsync<Sprite>(spriteLocation).Completed += OnGotSprite;
         }
+
+        private void OnGotIcon(UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle<Sprite> icon) { _icon = icon.Result; }
+        private void OnGotPic(UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle<Sprite> pic) { _picture = pic.Result; }
+        private void OnGotSprite(UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle<Sprite> sprite) { _sprite = sprite.Result; }
 
         private Sprite SpriteFromFile(string filepath)
         {
@@ -29,7 +51,6 @@ namespace InventorySystem
 
             return r;
         }
-
         private Texture2D TextureFromFile(string filepath)
         {
             Texture2D texture;
@@ -49,48 +70,59 @@ namespace InventorySystem
         #region attributes
 
         #region defaults
-        private const string _defaultItemName = "doo-dad";
-        private const string _defaultItemDescription = "This sure is neat, but also usless...";
+        private const string _defaultName = "doo-dad";
+        private const string _defaultDescription = "This sure is neat, but also usless...";
 
-        private const int _defaultItemValue = 0;
-        private const int _defaultItemUses = -1;
+        private const int _defaultValue = 0;
+        private const int _defaultUses = -1;
 
-        //private const Sprite _defaultItemIcon;
-        //private const Sprite _defaultItemPicture;
+        private const string _defaultIconLocation = "Assets / Scripts / Inventory / Default Images/flower.tif";
+        private const string _defaultPictureLocation = "Assets / Scripts / Inventory / Default Images/flower.tif";
+        private const string _defaultSpriteLocation = "Assets / Scripts / Inventory / Default Images/flower.tif";
 
-        private const bool _defaultItemUseable = true;
-        private const bool _defaultItemRemovable = true;
-        private const bool _defaultItemDestroyable = true;
+        private const bool _defaultUseable = true;
+        private const bool _defaultRemovable = true;
+        private const bool _defaultDestroyable = true;
 
-        //private const Func<void, void> _defaultItemEffect;
+        //private const Func<void, void> _defaultEffect;
         #endregion
 
         #region fields
-        private string _itemName;
-        private string _itemDescription;
+        private string _name;
+        private string _description;
 
-        private int _itemValue;
-        private int _itemUses;
+        private int _value;
+        private int _uses;
 
-        private Sprite _itemIcon;
-        private Sprite _itemPicture;
+        //private string _iconLocation;
+        //private string _pictureLocation;
+        //private string _spriteLocation;
 
-        private bool _itemUseable;
-        private bool _itemRemovable;
-        private bool _itemDestroyable;
+        private Sprite _icon;
+        private Sprite _picture;
+        private Sprite _sprite;
 
-        //private function _itemEffect;
+        private bool _useable;
+        private bool _removable;
+        private bool _destroyable;
+
+        //private function _Effect;
         #endregion
 
         #region properties
-        public string ItemName { get => _itemName; }
-        public string ItemDescription { get => _itemDescription; }
+        public string Name { get => _name; }
+        public string Description { get => _description; }
 
-        public int ItemValue { get => _itemValue; }
+        public int Value { get => _value; }
+        public int Uses { get => _uses; }
         
-        public bool ItemUsable { get => _itemUseable; }
-        public bool ItemRemovable { get => _itemRemovable; }
-        public bool ItemDestroyable { get => _itemDestroyable; }
+        public Sprite Icon { get => _icon; }
+        public Sprite Picture { get => _picture; }
+        public Sprite Sprite { get => _sprite; }
+
+        public bool Usable { get => _useable; }
+        public bool Removable { get => _removable; }
+        public bool Destroyable { get => _destroyable; }
         #endregion
 
         #endregion
