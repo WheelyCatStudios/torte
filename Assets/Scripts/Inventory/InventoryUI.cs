88a4aabc9c639f149d06bb82378c35cdc4d7b24c
Jordan T. Gray <business@shinkson47.in>
2021-01-14 17:19:42 +0000

    Extracted UI element creation logic, removing ~13 duped lines
    Simplifies creating objects with canvas renderers and optionally background images.
    
    A single container holds both the GameObject holding the UI element, AND the RectTransform so there's no longer a need to create and store it separately.

--- Assets/Scripts/Inventory/InventoryUI.cs
+++ Assets/Scripts/Inventory/InventoryUI.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 
@@ -71,31 +72,25 @@ namespace InventorySystem
 		/// </summary>
 		
 		// [Obselete("Pop-ups should be provided with some information")] // I want to mark this as obsolete, but the attribute can't be located on mac. Seems to be a known bug.
-		public static GameObject MakePopUp() => MakePopUp(_defaultPopupName);
-        public static GameObject MakePopUp(string objName) => MakePopUp(objName, _defaultPopupMessage);
-        public static GameObject MakePopUp(string objName, string message) => MakePopUp(objName, new string[1] {message});
-        public static GameObject MakePopUp(string objName, string[] messages)
+		public static RectGO MakePopUp() => MakePopUp(_defaultPopupName);
+        public static RectGO MakePopUp(string objName) => MakePopUp(objName, _defaultPopupMessage);
+        public static RectGO MakePopUp(string objName, string message) => MakePopUp(objName, new string[1] {message});
+        public static RectGO MakePopUp(string objName, string[] messages)
         {
-            GameObject _PopUpUI = new GameObject(objName); 											// Create UI object
-            RectTransform _tempRectTrasform = _PopUpUI.AddComponent<RectTransform>(); 				// Add and get a new recttransform to size the UI. 
-			_tempRectTrasform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, _UIWidth);	// Assign width and height
-            _tempRectTrasform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, _UIHeight);
+            RectGO _PopUpUI = new RectGO(objName, true); 											// Create UI object with a background.
+			_PopUpUI.rect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, _UIWidth);	// Assign width and height
+            _PopUpUI.rect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, _UIHeight);
 			
-			SetAnchoredChild(_tempRectTrasform, GameObject.Find(_MainCameraName), _UIHeight, _UIWidth, Vector2.zero); // Place the UI under the camera within the UI. NB : This also scales the UI.
+			SetAnchoredChild(_PopUpUI.rect, GameObject.Find(_MainCameraName), _UIHeight, _UIWidth, Vector2.zero); // Place the UI under the camera within the UI. NB : This also scales the UI.
+			_PopUpUI.go.AddComponent<Canvas>().sortingOrder = 1; 										// Assign sorting order to render this UI above all other objects // TODO this should be a const
             
-			_PopUpUI.AddComponent<Canvas>().sortingOrder = 1; 										// Assign sorting order to render this UI above all other objects // TODO this should be a const
-            _PopUpUI.AddComponent<CanvasRenderer>();												// Create a component to render our canvas.
-            _PopUpUI.AddComponent<UnityEngine.UI.Image>().color = _UIBGColor; 						// Add a the background image, for now it's just a plain color.
-
             float _elementStack = 0, _elementHeight= 2;												// Variables used to correctly place multiple elements.
             foreach(string message in messages)
             {
-                GameObject _tempObj = new GameObject($"Text: {message.Substring(0, _previewTextLength)}" + ((message.Length > _previewTextLength) ? "..." : ""));
-                _tempObj.AddComponent<CanvasRenderer>();											// TODO do we need a canvas renderer for every TMP? if so, should this be moved into CreateTMP?
-                _tempRectTrasform = _tempObj.AddComponent<RectTransform>();							// Create canvas renderer and rect transform for the label.
-				SetAnchoredChild(_tempRectTrasform, _PopUpUI, _elementHeight, _UIWidth, new Vector2(0,0-(_elementStack+(_elementHeight/2)))); // Anchor the label to the UI, according to the element's position.
+                RectGO _tempObj = new RectGO($"Text: {message.Substring(0, _previewTextLength)}" + ((message.Length > _previewTextLength) ? "..." : ""));
+				SetAnchoredChild(_tempObj.rect, _PopUpUI.go, _elementHeight, _UIWidth, new Vector2(0,0-(_elementStack+(_elementHeight/2)))); // Anchor the label to the UI, according to the element's position.
 
-				createTMP(_tempObj, message);														// Add the text label to the anchored object.
+				createTMP(_tempObj.go, message);														// Add the text label to the anchored object.
             }
                
             return _PopUpUI;																		// Return the created pop-up UI.
@@ -107,28 +102,20 @@ namespace InventorySystem
 		/// <param name="inventory">The inventory to display.</param>
         private static void GenerateInventoryPopUp(Inventory inventory)
         {
-            GameObject _inventoryUI = MakePopUp(_InventoryObjectName, _UITitle);		// Create a pop-up to use for the inventory's UI.
-			RectTransform _mainTransform = _inventoryUI.GetComponent<RectTransform>();	// Fetch the main inventory UI's transform for use with placing objects.
-			
-																						// NB : volatile vars for placing objects.
-			RectTransform _tempRectTrasform = _mainTransform;							// Temporary container for the recttransform that we're currently working on. For now, the inventory UI's.
-			GameObject _tempObj;														// Temporary object container for use when adding objects. For now, null.
+            RectGO _inventoryUI = MakePopUp(_InventoryObjectName, _UITitle);			// Create a pop-up to use for the inventory's UI.
+
+			RectGO _tempObj;															// Volatile temporary object container for use when adding objects. For now, null.
             float _elementStack = 2, _elementHeight = 2;								// Vars that define the placement of objects, according to thier place in the list.
             
-            GameObject _scroller = new GameObject("Scroller");							// Create an object to use for a scrollable container
-            _tempRectTrasform = _scroller.AddComponent<RectTransform>();				// Create and get a rect transform
+            RectGO _scroller = new RectGO("Scroller", true);							// Create an object to use for a scrollable container
 																						// Set it as an anchored child of the inventory UI
-			SetAnchoredChild(_tempRectTrasform, _inventoryUI, (_UIHeight-_elementStack), _UIWidth, new Vector2(0, 0 - (_elementStack + ((_UIHeight - _elementStack) / 2))), false);
+			SetAnchoredChild(_scroller.rect, _inventoryUI.go, (_UIHeight-_elementStack), _UIWidth, new Vector2(0, 0 - (_elementStack + ((_UIHeight - _elementStack) / 2))), false);
+            ScrollRect _scrollRect = _scroller.go.AddComponent<ScrollRect>(); 			// Add a scroll rect to the object.
+            _scroller.go.AddComponent<Mask>();											// Add a mask?
 
-            UnityEngine.UI.ScrollRect _scrollRect = _scroller.AddComponent<UnityEngine.UI.ScrollRect>(); // Add a scroll rect to the object.
-            _scroller.AddComponent<CanvasRenderer>();													 // Add a canvas renderer to the scroll pane.
-            _scroller.AddComponent<UnityEngine.UI.Image>().color = new Color(0,0,0,0.005f);				 // Set the background color of the scroll pane
-            _scroller.AddComponent<UnityEngine.UI.Mask>();												 // Add a mask?
-
-            GameObject _scrollContainer = new GameObject("Scroll Container");			// Container to house the objects in the scroll pane.
-            _tempRectTrasform = _scrollContainer.AddComponent<RectTransform>();			
+            RectGO _scrollContainer = new RectGO("Scroll Container");					// Container to house the objects in the scroll pane.
 																						// Set as a child of the scroll pane.
-            SetAnchoredChild(_tempRectTrasform, _scroller, _UIWidth, (inventory.InventoryItems.Count * _elementHeight), Vector2.zero, false);
+            SetAnchoredChild(_scrollContainer.rect, _scroller.go, _UIWidth, (inventory.InventoryItems.Count * _elementHeight), Vector2.zero, false);
         
 			_elementStack = 0;															// Reset element stack.
 
@@ -136,32 +123,58 @@ namespace InventorySystem
             foreach(InventoryItem currentItem in inventory.InventoryItems)				// for every item in the inventory:
             {
                 //Item container
-                _tempObj = new GameObject($"Item: {currentItem.Name}");					// Create a Game Object to represent it,
-                _tempRectTrasform = _tempObj.AddComponent<RectTransform>();				// Add a rect, and add it as a child to the scroll able container.
-				SetAnchoredChild(_tempRectTrasform, _scrollContainer, _elementHeight, _UIWidth, new Vector2(0, 0 - (_elementStack + (_elementHeight / 2))), false);
+                _tempObj = new RectGO($"Item: {currentItem.Name}");						// Create a Game Object to represent it,
+ 				SetAnchoredChild(_tempObj.rect, _scrollContainer.go, _elementHeight, _UIWidth, new Vector2(0, 0 - (_elementStack + (_elementHeight / 2))), false);
 
                 //item icon
-                _tempRectTrasform = new GameObject("Icon").AddComponent<RectTransform>(); // Create an object to display the item's icon, and add it to the above GO.
-				SetAnchoredChild(_tempRectTrasform, _tempObj, _elementHeight, _elementHeight, new Vector2(_elementHeight/2,0-(_elementHeight/2)), false, Vector2.up, Vector2.up);
-                _tempRectTrasform.gameObject.AddComponent<UnityEngine.UI.Image>().sprite = currentItem.Icon; // Create an image, and assign it the icon of the current item.
-
+                RectGO _icon =  new RectGO("Icon"); 									// Create an object to display the item's icon, and add it to the above GO.
+				SetAnchoredChild(_icon.rect, _tempObj.go, _elementHeight, _elementHeight, new Vector2(_elementHeight/2,0-(_elementHeight/2)), false, Vector2.up, Vector2.up);
+                _icon.rect.gameObject.AddComponent<UnityEngine.UI.Image>().sprite = currentItem.Icon; // Create an image, and assign it the icon of the current item.
+				
 				//item text
-				GameObject _label = new GameObject("Label");								// create an object to display the item's name, and add it to the above item container.
-                _tempRectTrasform = _label.AddComponent<RectTransform>();
-				SetAnchoredChild(_tempRectTrasform, _tempObj, _elementHeight, _mainTransform.sizeDelta.x , new Vector2(_elementHeight * 3, 0-(_elementHeight/2)), false, Vector2.up, Vector2.up);
-				createTMP(_label, currentItem.Name);										// Add a TMP label with the item's text.
+				RectGO _label = new RectGO("Label");									// create an object to display the item's name, and add it to the above item container.
+                SetAnchoredChild(_label.rect, _tempObj.go, _elementHeight, _inventoryUI.rect.sizeDelta.x , new Vector2(_elementHeight * 3, 0-(_elementHeight/2)), false, Vector2.up, Vector2.up);
+				createTMP(_label.go, currentItem.Name);									// Add a TMP label with the item's text.
 
                 _elementStack += _elementHeight;
             }
 
             //Link-up
-            _scrollRect.content = _scrollContainer.GetComponent<RectTransform>();		
-            _inventoryUI.AddComponent<UnityEngine.EventSystems.EventSystem>();
-            _inventoryUI.AddComponent<UnityEngine.EventSystems.BaseInput>();
-            _inventoryUI.AddComponent<UnityEngine.InputSystem.UI.InputSystemUIInputModule>();
+            _scrollRect.content = _scrollContainer.rect;		
+            _inventoryUI.go.AddComponent<UnityEngine.EventSystems.EventSystem>();
+            _inventoryUI.go.AddComponent<UnityEngine.EventSystems.BaseInput>();
+            _inventoryUI.go.AddComponent<UnityEngine.InputSystem.UI.InputSystemUIInputModule>();
         }
 
 		#region utility
+		/// <summary>
+		/// A simple container that creates and stores a game object
+		/// with a RectTransform, CanvasRenderer, and optionally a background image.
+		/// </summary>
+		public class RectGO {
+			/// <summary>
+			/// The GameObject that this RectGO created.
+			/// </summary>
+			public GameObject go;
+
+			/// <summary>
+			/// The RectTransform of the GameObject this RectGO created.
+			/// </summary>
+			public RectTransform rect;
+
+			public RectGO(string s, bool colorbg) 		=>	init(new GameObject(s), colorbg);
+			public RectGO(string s)						=>	init(new GameObject(s), false);
+			public RectGO(GameObject _go, bool colorbg)	=>	init(_go, colorbg);
+			public RectGO(GameObject _go) 				=>	init(_go, false);
+
+			private void init(GameObject _go, bool colorbg){
+				go = _go;
+				rect = go.AddComponent<RectTransform>();
+				go.AddComponent<CanvasRenderer>();												// Create a component to render our canvas.
+            	if (colorbg) go.AddComponent<UnityEngine.UI.Image>().color = _UIBGColor; 		// Add a the background image, for now it's just a plain color.
+			}
+		}
+
 		private static void SetAnchoredChild(RectTransform _rectTransform, GameObject _parent, float verticalSize, float horizontalSize, Vector2 _anchorPos) => 
 		SetAnchoredChild( _rectTransform, _parent, verticalSize, horizontalSize, _anchorPos, true);
 
