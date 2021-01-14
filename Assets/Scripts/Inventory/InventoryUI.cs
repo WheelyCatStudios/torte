dffd864d92ca2695068a1d6202432b655f739115
Jordan T. Gray <business@shinkson47.in>
2021-01-14 17:21:44 +0000

    Converted all indentation to TAB

--- Assets/Scripts/Inventory/InventoryUI.cs
+++ Assets/Scripts/Inventory/InventoryUI.cs
@@ -9,8 +9,8 @@ namespace InventorySystem
 	/// Static utility class that creates a UI which displays and interacts
 	/// with a provided Inventory.
 	/// </summary>
-    public class InventoryUI
-    {
+	public class InventoryUI
+	{
 		#region properies
 		/// <summary>
 		/// Controls the general scale of the UI.
@@ -51,7 +51,7 @@ namespace InventorySystem
 		/// <summary>
 		/// Fallback message for pop-ups with no provided message
 		/// </summary>
-        private const string _defaultPopupMessage = "No message...";
+		private const string _defaultPopupMessage = "No message...";
 
 		/// <summary>
 		/// The name given to pop up objects.
@@ -64,7 +64,7 @@ namespace InventorySystem
 		/// The permitted length for TMP text within object labels.<br/>
 		/// Text longer than this value will be cut with '...'
 		/// </summary>
-        private const int _previewTextLength = 5;
+		private const int _previewTextLength = 5;
 		#endregion properties
 
 		/// <summary>
@@ -73,78 +73,78 @@ namespace InventorySystem
 		
 		// [Obselete("Pop-ups should be provided with some information")] // I want to mark this as obsolete, but the attribute can't be located on mac. Seems to be a known bug.
 		public static RectGO MakePopUp() => MakePopUp(_defaultPopupName);
-        public static RectGO MakePopUp(string objName) => MakePopUp(objName, _defaultPopupMessage);
-        public static RectGO MakePopUp(string objName, string message) => MakePopUp(objName, new string[1] {message});
-        public static RectGO MakePopUp(string objName, string[] messages)
-        {
-            RectGO _PopUpUI = new RectGO(objName, true); 											// Create UI object with a background.
+		public static RectGO MakePopUp(string objName) => MakePopUp(objName, _defaultPopupMessage);
+		public static RectGO MakePopUp(string objName, string message) => MakePopUp(objName, new string[1] {message});
+		public static RectGO MakePopUp(string objName, string[] messages)
+		{
+			RectGO _PopUpUI = new RectGO(objName, true); 											// Create UI object with a background.
 			_PopUpUI.rect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, _UIWidth);	// Assign width and height
-            _PopUpUI.rect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, _UIHeight);
+			_PopUpUI.rect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, _UIHeight);
 			
 			SetAnchoredChild(_PopUpUI.rect, GameObject.Find(_MainCameraName), _UIHeight, _UIWidth, Vector2.zero); // Place the UI under the camera within the UI. NB : This also scales the UI.
 			_PopUpUI.go.AddComponent<Canvas>().sortingOrder = 1; 										// Assign sorting order to render this UI above all other objects // TODO this should be a const
-            
-            float _elementStack = 0, _elementHeight= 2;												// Variables used to correctly place multiple elements.
-            foreach(string message in messages)
-            {
-                RectGO _tempObj = new RectGO($"Text: {message.Substring(0, _previewTextLength)}" + ((message.Length > _previewTextLength) ? "..." : ""));
+			
+			float _elementStack = 0, _elementHeight= 2;												// Variables used to correctly place multiple elements.
+			foreach(string message in messages)
+			{
+				RectGO _tempObj = new RectGO($"Text: {message.Substring(0, _previewTextLength)}" + ((message.Length > _previewTextLength) ? "..." : ""));
 				SetAnchoredChild(_tempObj.rect, _PopUpUI.go, _elementHeight, _UIWidth, new Vector2(0,0-(_elementStack+(_elementHeight/2)))); // Anchor the label to the UI, according to the element's position.
 
 				createTMP(_tempObj.go, message);														// Add the text label to the anchored object.
-            }
-               
-            return _PopUpUI;																		// Return the created pop-up UI.
-        }
+			}
+			   
+			return _PopUpUI;																		// Return the created pop-up UI.
+		}
 
 		/// <summary>
 		/// Populates a Pop-Up with inventory items.
 		/// </summary>
 		/// <param name="inventory">The inventory to display.</param>
-        private static void GenerateInventoryPopUp(Inventory inventory)
-        {
-            RectGO _inventoryUI = MakePopUp(_InventoryObjectName, _UITitle);			// Create a pop-up to use for the inventory's UI.
+		private static void GenerateInventoryPopUp(Inventory inventory)
+		{
+			RectGO _inventoryUI = MakePopUp(_InventoryObjectName, _UITitle);			// Create a pop-up to use for the inventory's UI.
 
 			RectGO _tempObj;															// Volatile temporary object container for use when adding objects. For now, null.
-            float _elementStack = 2, _elementHeight = 2;								// Vars that define the placement of objects, according to thier place in the list.
-            
-            RectGO _scroller = new RectGO("Scroller", true);							// Create an object to use for a scrollable container
+			float _elementStack = 2, _elementHeight = 2;								// Vars that define the placement of objects, according to thier place in the list.
+			
+			RectGO _scroller = new RectGO("Scroller", true);							// Create an object to use for a scrollable container
 																						// Set it as an anchored child of the inventory UI
 			SetAnchoredChild(_scroller.rect, _inventoryUI.go, (_UIHeight-_elementStack), _UIWidth, new Vector2(0, 0 - (_elementStack + ((_UIHeight - _elementStack) / 2))), false);
-            ScrollRect _scrollRect = _scroller.go.AddComponent<ScrollRect>(); 			// Add a scroll rect to the object.
-            _scroller.go.AddComponent<Mask>();											// Add a mask?
+			ScrollRect _scrollRect = _scroller.go.AddComponent<ScrollRect>(); 			// Add a scroll rect to the object.
+			_scroller.go.AddComponent<Mask>();											// Add a mask?
 
-            RectGO _scrollContainer = new RectGO("Scroll Container");					// Container to house the objects in the scroll pane.
+			RectGO _scrollContainer = new RectGO("Scroll Container");					// Container to house the objects in the scroll pane.
 																						// Set as a child of the scroll pane.
-            SetAnchoredChild(_scrollContainer.rect, _scroller.go, _UIWidth, (inventory.InventoryItems.Count * _elementHeight), Vector2.zero, false);
-        
+			SetAnchoredChild(_scrollContainer.rect, _scroller.go, _UIWidth, (inventory.InventoryItems.Count * _elementHeight), Vector2.zero, false);
+		
 			_elementStack = 0;															// Reset element stack.
 
-            //Items
-            foreach(InventoryItem currentItem in inventory.InventoryItems)				// for every item in the inventory:
-            {
-                //Item container
-                _tempObj = new RectGO($"Item: {currentItem.Name}");						// Create a Game Object to represent it,
+			//Items
+			foreach(InventoryItem currentItem in inventory.InventoryItems)				// for every item in the inventory:
+			{
+				//Item container
+				_tempObj = new RectGO($"Item: {currentItem.Name}");						// Create a Game Object to represent it,
  				SetAnchoredChild(_tempObj.rect, _scrollContainer.go, _elementHeight, _UIWidth, new Vector2(0, 0 - (_elementStack + (_elementHeight / 2))), false);
 
-                //item icon
-                RectGO _icon =  new RectGO("Icon"); 									// Create an object to display the item's icon, and add it to the above GO.
+				//item icon
+				RectGO _icon =  new RectGO("Icon"); 									// Create an object to display the item's icon, and add it to the above GO.
 				SetAnchoredChild(_icon.rect, _tempObj.go, _elementHeight, _elementHeight, new Vector2(_elementHeight/2,0-(_elementHeight/2)), false, Vector2.up, Vector2.up);
-                _icon.rect.gameObject.AddComponent<UnityEngine.UI.Image>().sprite = currentItem.Icon; // Create an image, and assign it the icon of the current item.
+				_icon.rect.gameObject.AddComponent<UnityEngine.UI.Image>().sprite = currentItem.Icon; // Create an image, and assign it the icon of the current item.
 				
 				//item text
 				RectGO _label = new RectGO("Label");									// create an object to display the item's name, and add it to the above item container.
-                SetAnchoredChild(_label.rect, _tempObj.go, _elementHeight, _inventoryUI.rect.sizeDelta.x , new Vector2(_elementHeight * 3, 0-(_elementHeight/2)), false, Vector2.up, Vector2.up);
+				SetAnchoredChild(_label.rect, _tempObj.go, _elementHeight, _inventoryUI.rect.sizeDelta.x , new Vector2(_elementHeight * 3, 0-(_elementHeight/2)), false, Vector2.up, Vector2.up);
 				createTMP(_label.go, currentItem.Name);									// Add a TMP label with the item's text.
 
-                _elementStack += _elementHeight;
-            }
+				_elementStack += _elementHeight;
+			}
 
-            //Link-up
-            _scrollRect.content = _scrollContainer.rect;		
-            _inventoryUI.go.AddComponent<UnityEngine.EventSystems.EventSystem>();
-            _inventoryUI.go.AddComponent<UnityEngine.EventSystems.BaseInput>();
-            _inventoryUI.go.AddComponent<UnityEngine.InputSystem.UI.InputSystemUIInputModule>();
-        }
+			//Link-up
+			_scrollRect.content = _scrollContainer.rect;		
+			_inventoryUI.go.AddComponent<UnityEngine.EventSystems.EventSystem>();
+			_inventoryUI.go.AddComponent<UnityEngine.EventSystems.BaseInput>();
+			_inventoryUI.go.AddComponent<UnityEngine.InputSystem.UI.InputSystemUIInputModule>();
+		}
 
 		#region utility
 		/// <summary>
@@ -171,7 +171,7 @@ namespace InventorySystem
 				go = _go;
 				rect = go.AddComponent<RectTransform>();
 				go.AddComponent<CanvasRenderer>();												// Create a component to render our canvas.
-            	if (colorbg) go.AddComponent<UnityEngine.UI.Image>().color = _UIBGColor; 		// Add a the background image, for now it's just a plain color.
+				if (colorbg) go.AddComponent<UnityEngine.UI.Image>().color = _UIBGColor; 		// Add a the background image, for now it's just a plain color.
 			}
 		}
 
@@ -185,12 +185,12 @@ namespace InventorySystem
 			SetAnchoredChild( _rectTransform, _parent, verticalSize, horizontalSize, _anchorPos, true,  min, max);
 
 		private static void SetAnchoredChild(RectTransform _rectTransform, GameObject _parent, float verticalSize, float horizontalSize, Vector2 _anchorPos, bool doScale, Vector2 min, Vector2 max){
-            _rectTransform.SetParent(_parent.transform);
-            _rectTransform.anchorMin = min;
-            _rectTransform.anchorMax = max;
-            _rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, verticalSize);
-            _rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, horizontalSize);
-            _rectTransform.anchoredPosition = _anchorPos;
+			_rectTransform.SetParent(_parent.transform);
+			_rectTransform.anchorMin = min;
+			_rectTransform.anchorMax = max;
+			_rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, verticalSize);
+			_rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, horizontalSize);
+			_rectTransform.anchoredPosition = _anchorPos;
 			_rectTransform.localScale = (doScale) ? _UIScale : new Vector3(1,1,1);
 		}
 
@@ -239,23 +239,23 @@ namespace InventorySystem
 		}
 
 		private static TextMeshProUGUI createTMP(GameObject go, string s){
-                TextMeshProUGUI _tempTMP = go.AddComponent<TextMeshProUGUI>();
-                _tempTMP.text = s;
-                _tempTMP.alignment = TextAlignmentOptions.Center;
-                _tempTMP.fontSizeMin = 1;
-                _tempTMP.enableAutoSizing = true;
+				TextMeshProUGUI _tempTMP = go.AddComponent<TextMeshProUGUI>();
+				_tempTMP.text = s;
+				_tempTMP.alignment = TextAlignmentOptions.Center;
+				_tempTMP.fontSizeMin = 1;
+				_tempTMP.enableAutoSizing = true;
 				return _tempTMP;
 		}
 
-        public static void OpenInventory(Inventory targetInventory)
-        {
-            Debug.Log("UwU - Someone opened up an inventory!");
+		public static void OpenInventory(Inventory targetInventory)
+		{
+			Debug.Log("UwU - Someone opened up an inventory!");
 
-            GenerateInventoryPopUp(targetInventory);
-            //MakePopUp("Sup dude? Hows the testing going?");
-            //MakePopUp(new string[2]{ "This is a test","this is another fucking test"});
-            //I have no fucking clue
-        }
+			GenerateInventoryPopUp(targetInventory);
+			//MakePopUp("Sup dude? Hows the testing going?");
+			//MakePopUp(new string[2]{ "This is a test","this is another fucking test"});
+			//I have no fucking clue
+		}
 		#endregion utility
-    }
+	}
 }
