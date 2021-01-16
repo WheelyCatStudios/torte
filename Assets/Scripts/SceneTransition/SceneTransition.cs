dbee2c9a771d26e5bbf094414137deaff24c3e7b
Sean Lynch <66145294+funisinfinite@users.noreply.github.com>
2021-01-16 15:10:25 -0500

    Scene transition story (#45)
    * Added empty game object to trigger scene transitions
    
    * Created SceneTransition Script with Debug test
    
    * Added player tag to Entity
    
    * Added a empty scene to test transitions
    
    * Added simple base scene transition code
    
    * Added new scene to build settings
    
    * Started adding variables
    
    * Added direction and updated approachThreshold
    
    * Added distance calcuation
    
    * Start on async code routine
    
    * Added bool to control loading
    
    * Added unloading script
    
    * Added canTransition flag
    
    * Added code to count for colliders
    
    Added this so that if needed players will be blocked from procedding out of bounds if the canTransition flag is false
    
    * Added TODO comments
    
    * Update TestingGround.unity
    
    * Removed Unloading Script
    
    * hasLoadAsync change
    
    * Quartzi Script
    
    * Removed unload else if
    
    * Added a folder to organize

---
+++ Assets/Scripts/SceneTransition/SceneTransition.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+    public string sceneName; //Required Field; Case Sensitve; Please use full path instead of name
+    public float approachThreshold = 5f;
+    public bool canTransition = true;
+    private float departThreshold;
+    private Rigidbody2D playerRb;
+    private float distance;
+
+    private AsyncOperation asyncLoad;
+    private bool hasLoadAsync = false;
+
+
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        departThreshold = approachThreshold * 1.5f;
+        playerRb = GameObject.FindWithTag("Player").GetComponent<Rigidbody2D>();
+    }
+
+    void LateUpdate() {
+        
+        distance = playerRb.Distance(gameObject.GetComponent<BoxCollider2D>()).distance; // Calucates distance to trigger
+        if (distance < approachThreshold & !hasLoadAsync)
+        {
+            asyncLoad = SceneManager.LoadSceneAsync(sceneName);
+            asyncLoad.allowSceneActivation = false;
+            hasLoadAsync = true;
+        }
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player") & canTransition)
+        {
+            hasLoadAsync = false;
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player") & canTransition)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+
+    /*IEnumerator LoadSceneAsync()
+    {
+
+        asyncLoad = SceneManager.LoadSceneAsync(sceneName);
+        asyncLoad.allowSceneActivation = false;
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            Debug.Log(asyncLoad.progress);
+            yield return null;
+        }
+    }*/
+    
+}
