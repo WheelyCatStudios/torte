040c3da0b92138c875ceea0cd10494e07f4fae6d
Jordan T. Gray <business@shinkson47.in>
2021-02-21 17:02:34 +0000

    Created a state machine demo script
    It cycles through every state to demo all bubbles available.

---
+++ Assets/Sprites/UI/BubbleDemo.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class BubbleDemo : StateMachineBehaviour
+{
+	public int MaxStates = 7;
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+		int v = animator.GetInteger("State");
+		animator.SetBool("PlayerClose", !animator.GetBool("PlayerClose"));
+		if (animator.GetBool("PlayerClose")) return;
+		animator.SetInteger("State", (v == MaxStates) ? 0 : v + 1);
+    }
+}
